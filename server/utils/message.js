5ae938ed2807a17c08e46f183c5dcea4927e2a8e
Timeth Subasinghe <timethsws@gmail.com>
2018-06-11 03:26:05 +0530

    Forgot a : in the maps url

--- server/utils/message.js
+++ server/utils/message.js
@@ -9,7 +9,7 @@ var generateMessage = (from, text) => {
 var generateLocationMessage = (from, lat,lng) => {
     return {
         from,
-        url : `https//www.google.com/maps?q=${lat},${lng}`,
+        url : `https://www.google.com/maps?q=${lat},${lng}`,
         createdAt: new Date().getTime()
     }
 }
